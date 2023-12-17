46777255b0ba29100698f6e51eaca64cda23020a
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-17 04:37:47 -0300

    minha aula de js

---
+++ aula-js/JS/aula01.js
@@ -0,0 +1,29 @@
+//selecionando um elementos atraves do js essa sao as as 4 forma
+
+document.getElementById('texto')
+.innerHTML = 'ola mundo meu primeiro texto <b>JS</b>';
+
+//segundo metodo  
+
+document.write('esse e o segundo metodo document.write');
+
+// 3 metodo  com alerte
+
+alert(10 + 5);
+
+// 4 modo e o console.log
+
+console.log("oi isso e um console.log");
+
+
+//declaraçoes de javascript tudo que ta acima e uma declaraçaes semple usar ; para separar
+
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+/* Como escrever no js a nossa sintax*/
+
+console.log(5 + 5)//numero
+console.log("5 + 5")//texto string
