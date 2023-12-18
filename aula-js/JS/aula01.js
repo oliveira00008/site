3b846409479472fa5a1801fff558a5e76fd3285e
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-18 04:24:47 -0300

    atualisando meu exercicio de java secript

--- aula-js/JS/aula01.js
+++ aula-js/JS/aula01.js
@@ -1,29 +1,81 @@
 //selecionando um elementos atraves do js essa sao as as 4 forma
 
-document.getElementById('texto')
-.innerHTML = 'ola mundo meu primeiro texto <b>JS</b>';
+//document.getElementById('texto')
+//.innerHTML = 'ola mundo meu primeiro texto <b>JS</b>';
 
 //segundo metodo  
 
-document.write('esse e o segundo metodo document.write');
+//document.write('esse e o segundo metodo document.write');
 
 // 3 metodo  com alerte
 
-alert(10 + 5);
+// alert(10 + 5);
 
 // 4 modo e o console.log
 
-console.log("oi isso e um console.log");
+//console.log("oi isso e um console.log");
 
 
 //declaraçoes de javascript tudo que ta acima e uma declaraçaes semple usar ; para separar
 
-let a,b,c;
+/*let a,b,c;
 a=5;
 b=6;
 c=7;
+*/
 
 /* Como escrever no js a nossa sintax*/
 
-console.log(5 + 5)//numero
-console.log("5 + 5")//texto string
+//console.log(5 + 5)//numero
+//console.log("5 + 5")//texto string
+
+
+// AULA 6 DE JAVA SCRIPT
+
+//vamos emteder sobre as variavel 
+
+//No javascript temos 3 tipo de variavel a (var , let, const.).
+
+
+//var pote = "bombom"
+//alert("pote")
+
+/*var a = 3;
+var b = 5; 
+var c = 6;
+alert(a + (c * b ));
+*/
+
+//poderia fazer assim 
+
+//DECLARAÇAO DE VARIAVEL
+
+//var a,b,c ;
+
+//ATRIBUIÇAO DAS VARIAVEL POSSO REATRIBUIR VALORES 
+/*a = 3 ;
+b = 5;
+c = a + b;*/
+
+/*exemplo  var a = 7;*/
+//alert(a);
+
+//outro exemplo sobre a variavel (VAR)
+//comcatenizaçao de palavras
+
+/*var nome, sobreNome, nomeCompleto;
+
+nome = "Gabriel";
+sobreNome = "Alves";
+nomeCompleto = nome + " "+ sobreNome;
+
+document.getElementById("texto")
+.innerHTML = nomeCompleto;
+*/
+
+//oque siginifica (var)
+/*
+ var -> pode ser redeclarado a qual quer momento 
+ let --> vai pegar o valor uma ves e nao pode ser reatribuido  mas pode ter mudança em bloco {...}
+ const ---> Nao tem como mudar o valoer e aquele e jaera
+ */
